279b25b8f095af82624f33029021a57bca6c9f1d
febj38 <133888022+febj38@users.noreply.github.com>
2023-05-22 00:49:16 -0400

    Add files via upload
    3rd final game addition

---
+++ 2dVballCourt.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(_dVballCourt)
+
+//======================================================================
+//
+//	_dVballCourt, 256x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 480 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 15360 + 2048 = 17920
+//
+//	Time-stamp: 2022-12-08, 16:13:53
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT__DVBALLCOURT_H
+#define GRIT__DVBALLCOURT_H
+
+#define _dVballCourtTilesLen 15360
+extern const unsigned short _dVballCourtTiles[7680];
+
+#define _dVballCourtMapLen 2048
+extern const unsigned short _dVballCourtMap[1024];
+
+#define _dVballCourtPalLen 512
+extern const unsigned short _dVballCourtPal[256];
+
+#endif // GRIT__DVBALLCOURT_H
+
+//}}BLOCK(_dVballCourt)
